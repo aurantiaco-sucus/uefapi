df173e7e606287ef641bd05e2c39b7738ea9d104
AurantiacoSucus <30406783+aurantiaco-sucus@users.noreply.github.com>
2024-04-01 12:15:44 +0800

    daily

--- src/gfx2.rs
+++ src/gfx2.rs
@@ -1,4 +1,4 @@
-use alloc::rc::Rc;
+use alloc::vec;
 use alloc::vec::Vec;
 use core::ops::{Add, Sub};
 
@@ -8,7 +8,7 @@ pub struct Pos {
     pub y: i32,
 }
 
-pub fn pos(x: i32, y: i32) -> Pos {
+pub const fn pos(x: i32, y: i32) -> Pos {
     Pos { x, y }
 }
 
@@ -41,7 +41,7 @@ impl Sub for Pos {
 }
 
 impl Pos {
-    pub fn dim(self) -> Dim {
+    pub const fn dim(self) -> Dim {
         Dim { w: self.x, h: self.y }
     }
 }
@@ -52,7 +52,7 @@ pub struct Dim {
     pub h: i32,
 }
 
-pub fn dim(w: i32, h: i32) -> Dim {
+pub const fn dim(w: i32, h: i32) -> Dim {
     Dim { w, h }
 }
 
@@ -85,7 +85,7 @@ impl Sub for Dim {
 }
 
 impl Dim {
-    pub fn pos(self) -> Pos {
+    pub const fn pos(self) -> Pos {
         Pos { x: self.w, y: self.h }
     }
 }
@@ -96,7 +96,7 @@ pub struct Rect {
     pub dim: Dim,
 }
 
-pub fn rect(pos: Pos, dim: Dim) -> Rect {
+pub const fn rect(pos: Pos, dim: Dim) -> Rect {
     Rect { pos, dim }
 }
 
@@ -141,7 +141,7 @@ pub struct Area {
     pub pos2: Pos,
 }
 
-pub fn area(pos1: Pos, pos2: Pos) -> Area {
+pub const fn area(pos1: Pos, pos2: Pos) -> Area {
     Area { pos1, pos2 }
 }
 
@@ -174,32 +174,108 @@ pub struct Color {
     pub a: u8
 }
 
-pub fn rgba(r: u8, g: u8, b: u8, a: u8) -> Color {
-    Color { r, g, b, a }
+pub const fn rgba(r: u8, g: u8, b: u8, a: u8) -> Color {
+    let r = r as u32 * a as u32 / 255;
+    let g = g as u32 * a as u32 / 255;
+    let b = b as u32 * a as u32 / 255;
+    Color {
+        r: r as u8,
+        g: g as u8,
+        b: b as u8,
+        a,
+    }
+}
+
+pub const fn rgb(r: u8, g: u8, b: u8) -> Color {
+    rgba(r, g, b, 255)
 }
 
 impl Color {
-    pub fn with_alpha(self, a: u8) -> Color {
+    pub const fn black_alpha(a: u8) -> Self {
+        rgba(0, 0, 0, a)
+    }
+    
+    pub const fn white_alpha(a: u8) -> Self {
+        rgba(255, 255, 255, a)
+    }
+    
+    pub const BLACK: Self = rgb(0, 0, 0);
+    pub const WHITE: Self = rgb(255, 255, 255);
+    pub const RED: Self = rgb(255, 0, 0);
+    pub const GREEN: Self = rgb(0, 255, 0);
+    pub const BLUE: Self = rgb(0, 0, 255);
+}
+
+impl Color {
+    #[inline]
+    pub fn apply_alpha(self, alpha: u8) -> Self {
+        let oa = self.a as u32;
+        let na = self.a as u32 * alpha as u32 / 255;
+        let r = self.r as u32 * na / oa;
+        let g = self.g as u32 * na / oa;
+        let b = self.b as u32 * na / oa;
         Color {
-            r: self.r,
-            g: self.g,
-            b: self.b,
-            a,
+            r: r as u8,
+            g: g as u8,
+            b: b as u8,
+            a: na as u8,
+        }
+    }
+
+    #[inline]
+    pub fn premultiplied_over(self, other: Self) -> Self {
+        Self {
+            r: premultiplied_over_ch(self.r, other.r, other.a),
+            g: premultiplied_over_ch(self.g, other.g, other.a),
+            b: premultiplied_over_ch(self.b, other.b, other.a),
+            a: premultiplied_over_ch(self.a, other.a, other.a),
         }
     }
     
-    pub fn apply_alpha(self, alpha: u8) -> Color {
-        Color {
-            r: self.r,
-            g: self.g,
-            b: self.b,
-            a: (self.a as u32 * alpha as u32 / 255) as u8,
+    #[inline]
+    pub fn additive_over(self, other: Self) -> Self {
+        Self {
+            r: self.r.saturating_add(other.r),
+            g: self.g.saturating_add(other.g),
+            b: self.b.saturating_add(other.b),
+            a: self.a.saturating_add(other.a),
         }
     }
+}
+
+#[inline]
+fn premultiplied_over_ch(bg: u8, fg: u8, fg_alpha: u8) -> u8 {
+    ((fg as u32 + bg as u32 * (255 - fg_alpha as u32)) / 255) as u8
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Default)]
 pub struct Buffer {
-    data: Rc<Vec<Color>>,
-    dim: Dim,
+    pub data: Vec<Color>,
+    pub dim: Dim,
+}
+
+impl Buffer {
+    pub fn new(dim: Dim) -> Self {
+        Self {
+            data: vec![Color::BLACK; (dim.w * dim.h) as usize],
+            dim,
+        }
+    }
+    
+    pub fn new_cleared(dim: Dim, color: Color) -> Self {
+        Self {
+            data: vec![color; (dim.w * dim.h) as usize],
+            dim,
+        }
+    }
+    
+    pub fn rect(&self) -> Rect {
+        rect(pos(0, 0), self.dim)
+    }
+    
+    pub fn clear(&mut self, color: Color) {
+        for pixel in self.data.iter_mut() {
+            *pixel = color;
+        }
+    }
 }
